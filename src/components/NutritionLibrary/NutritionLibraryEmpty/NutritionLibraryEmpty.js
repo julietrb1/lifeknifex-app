6f75ac57f9b10d9be376141308c2caf3e045e0f0
Dylan Boyd <dylanjboyd@me.com>
2019-02-13 21:23:37 +1300

    Fix placeholder display on empty NutritionLibrary

--- src/components/NutritionLibrary/NutritionLibraryEmpty/NutritionLibraryEmpty.js
+++ src/components/NutritionLibrary/NutritionLibraryEmpty/NutritionLibraryEmpty.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import {Button, Divider, Icon, Image, Segment} from "semantic-ui-react";
+import {Button, Icon, Image, Segment} from "semantic-ui-react";
 import {Link} from "react-router-dom";
 import {COLOR_NUTRITION} from "../../../constants";
 import PropTypes from 'prop-types';
 
 const NutritionLibraryEmpty = props =>
-    <Segment placeholder>
-        <div className="placeholder-header">
-            <Image src='/img/undraw_pizza_sharing.svg' size='medium'/>
-            <h3>{props.isArchivedVisible
+    <Segment textAlign='center' placeholder>
+        <Image className="placeholder-image" src='/img/undraw_pizza_sharing.svg' size='medium'/>
+        <h3>{props.isArchivedVisible
                 ? 'No archived foods for you!'
                 : 'You don\'t have any foods yet.'}</h3>
-        </div>
-        <Divider hidden/>
         <Button animated='vertical' as={Link} to='/nutrition/library/new' color={COLOR_NUTRITION}>
             <Button.Content visible>Let&apos;s Create One</Button.Content>
             <Button.Content hidden>
@@ -22,7 +19,7 @@ const NutritionLibraryEmpty = props =>
     </Segment>;
 
 NutritionLibraryEmpty.propTypes = {
-    isArchivedVisible: PropTypes.boolean
+    isArchivedVisible: PropTypes.bool
 };
 
 export default NutritionLibraryEmpty;
