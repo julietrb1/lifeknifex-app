cf976bc3ec04e277032b1fd789eb7470f39e438e
Dylan Boyd <dylanjboyd@me.com>
2019-02-19 18:12:57 +1300

    Make buttons less pronounced in AnswerPre

--- src/components/GoalAnswer/AnswerPre/AnswerPre.js
+++ src/components/GoalAnswer/AnswerPre/AnswerPre.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import {Form} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import {COLOR_GOALS} from "../../../constants";
 
 const AnswerPre = props => {
     if (props.goal.style === 'yesno') {
         return <div>
-            <Form.Button fluid basic positive onClick={() => props.onAnswer(1)}>Yes</Form.Button>
-            <Form.Button fluid basic negative onClick={() => props.onAnswer(2)}>No</Form.Button>
+            <Form.Button fluid basic color={COLOR_GOALS} onClick={() => props.onAnswer(1)}>Yes</Form.Button>
+            <Form.Button fluid basic color={COLOR_GOALS} onClick={() => props.onAnswer(2)}>No</Form.Button>
         </div>;
     } else {
         return <div>
-            <Form.Button fluid basic positive onClick={() => props.onAnswer(1)}>Effectively</Form.Button>
-            <Form.Button fluid basic onClick={() => props.onAnswer(2)}>Adequately</Form.Button>
-            <Form.Button fluid basic onClick={() => props.onAnswer(3)}>Poorly</Form.Button>
-            <Form.Button fluid basic negative onClick={() => props.onAnswer(4)}>Unsuccessfully</Form.Button>
+            <Form.Button fluid basic color={COLOR_GOALS} onClick={() => props.onAnswer(1)}>Effectively</Form.Button>
+            <Form.Button fluid basic color={COLOR_GOALS} onClick={() => props.onAnswer(2)}>Adequately</Form.Button>
+            <Form.Button fluid basic color={COLOR_GOALS} onClick={() => props.onAnswer(3)}>Poorly</Form.Button>
+            <Form.Button fluid basic color={COLOR_GOALS} onClick={() => props.onAnswer(4)}>Unsuccessfully</Form.Button>
         </div>;
     }
 };
