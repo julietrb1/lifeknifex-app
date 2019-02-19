4390b9875f510b486e5dd4c6fe59a03c168d4a7e
Dylan Boyd <dylanjboyd@me.com>
2019-02-19 17:30:57 +1300

    Clean up AnswerPost

--- src/components/GoalAnswer/AnswerPost/AnswerPost.js
+++ src/components/GoalAnswer/AnswerPost/AnswerPost.js
@@ -2,56 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Form, Radio} from 'semantic-ui-react';
 
+const yesNoAnswerSet = [
+    {label: 'Yes', value: 1},
+    {label: 'No', value: 2},
+];
+
+const likertAnswerSet = [
+    {label: 'Effectively', value: 1},
+    {label: 'Adequately', value: 2},
+    {label: 'Poorly', value: 3},
+    {label: 'Unsuccessfully', value: 4},
+];
+
 const AnswerPost = props => {
-    if (props.goal.style === 'yesno') {
-        return <Form.Group inline>
-            <Form.Field
-                control={Radio}
-                label='Yes'
-                value='1'
-                name='goal-value'
-                checked={props.checkedValue === 1}
-                onChange={() => props.onAnswer(1)}/>
-            <Form.Field
-                control={Radio}
-                label='No'
-                value='2'
-                name='goal-value'
-                checked={props.checkedValue === 2}
-                onChange={() => props.onAnswer(2)}/>
-        </Form.Group>;
-    } else {
-        return <Form.Group inline>
-            <Form.Field
-                control={Radio}
-                label='Effectively'
-                value='1'
-                name='goal-value'
-                checked={props.checkedValue === 1}
-                onChange={() => props.onAnswer(1)}/>
-            <Form.Field
-                control={Radio}
-                label='Adequately'
-                value='2'
-                name='goal-value'
-                checked={props.checkedValue === 2}
-                onChange={() => props.onAnswer(2)}/>
-            <Form.Field
-                control={Radio}
-                label='Poorly'
-                value='3'
-                name='goal-value'
-                checked={props.checkedValue === 3}
-                onChange={() => props.onAnswer(3)}/>
-            <Form.Field
-                control={Radio}
-                label='Unsuccessfully'
-                value='4'
-                name='goal-value'
-                checked={props.checkedValue === 4}
-                onChange={() => props.onAnswer(4)}/>
-        </Form.Group>;
-    }
+    const answerSet = props.goal.style === 'yesno' ? yesNoAnswerSet : likertAnswerSet;
+    return <Form.Group inline>
+        {answerSet.map(item => <Form.Field
+            key={item.value}
+            control={Radio}
+            label={item.label}
+            value={item.value}
+            name='goal-value'
+            checked={props.checkedValue === item.value}
+            onChange={() => props.onAnswer(item.value)}/>)}
+    </Form.Group>;
 };
 
 AnswerPost.propTypes = {
