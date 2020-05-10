96d8a12697ab47992e0be2b6fc7435de586eec65
Dylan Boyd <dylanjboyd@icloud.com>
2020-05-10 16:45:33 +1200

    Use ID in place of URL

--- src/features/foods/foodSelectors.ts
+++ src/features/foods/foodSelectors.ts
@@ -1,7 +1,7 @@
 import {RootState} from "../../redux/rootReducer";
 
-export const selectFoodById = (state: RootState, foodId: number) => state.foodState.foodsById[foodId];
+export const selectFoodById = (state: RootState, foodId: number) => Object.values(state.foodState.foodsByUrl).find(f => f.id === foodId);
 export const selectFoodsLoaded = (state: RootState) => state.foodState.foodResponse !== null;
 export const selectFoodsLoading = (state: RootState) => state.foodState.isLoading;
-export const selectAllFoods = (state: RootState) => Object.values(state.foodState.foodsById);
+export const selectAllFoods = (state: RootState) => Object.values(state.foodState.foodsByUrl);
 export const selectFoodResponse = (state: RootState) => state.foodState.foodResponse;
