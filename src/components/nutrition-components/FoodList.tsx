f443f97b769bc5e943ab0d919cb5c4bbe79b20ea
Dylan Boyd <dylanjboyd@icloud.com>
2020-05-10 16:27:37 +1200

    Use URL as food key

--- src/components/nutrition-components/FoodList.tsx
+++ src/components/nutrition-components/FoodList.tsx
@@ -11,24 +11,26 @@ interface IFoodListProps {
     foods: IFood[];
 }
 
-const FoodList: React.FC<IFoodListProps> = props => <Card.Group>
-    {props.foods.map(food =>
-        <Card key={food.id} color={COLOR_NUTRITION}>
-            <Card.Content>
-                <FoodImage icon={food.icon}/>
-                <Card.Header>{food.name}</Card.Header>
-                <Card.Meta>{healthStrings[food.health_index - 1]}</Card.Meta>
-            </Card.Content>
-            <Card.Content extra>
-                <div>
-                    <Button size='tiny' basic as={Link} to={`/nutrition/log?food=${food.id}`}
-                            color={COLOR_NUTRITION}>Log</Button>
-                    <Button size='tiny' basic as={Link} to={`/nutrition/library/manage/${food.id}`}>Edit</Button>
-                </div>
-            </Card.Content>
-        </Card>
-    )}
-</Card.Group>;
+const FoodList: React.FC<IFoodListProps> = props => (
+    <Card.Group>
+        {props.foods.map(food =>
+            <Card key={food.url} color={COLOR_NUTRITION}>
+                <Card.Content>
+                    <FoodImage icon={food.icon}/>
+                    <Card.Header>{food.name}</Card.Header>
+                    <Card.Meta>{healthStrings[food.health_index - 1]}</Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                    <div>
+                        <Button size='tiny' basic as={Link} to={`/nutrition/log?food=${food.id}`}
+                                color={COLOR_NUTRITION}>Log</Button>
+                        <Button size='tiny' basic as={Link} to={`/nutrition/library/manage/${food.id}`}>Edit</Button>
+                    </div>
+                </Card.Content>
+            </Card>
+        )}
+    </Card.Group>
+);
 
 FoodList.propTypes = {
     foods: PropTypes.array.isRequired
