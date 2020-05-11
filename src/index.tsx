53afaf110aac20ba7d89d4f678c0101bd44220fc
Dylan Boyd <dylanjboyd@icloud.com>
2020-05-12 09:20:05 +1200

    Replace react-redux-toastr with notistack

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import store from "./redux/store";
 import {BrowserRouter as Router} from "react-router-dom";
-// import {ReduxToastr} from "react-redux-toastr/src/ReduxToastr";
+import {SnackbarProvider} from "notistack";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App/>
-        </Router>
-        {/*<ReduxToastr/>*/}
-    </Provider>
+    <SnackbarProvider maxSnack={1}>
+        <Provider store={store}>
+            <Router>
+                <App/>
+            </Router>
+        </Provider>
+    </SnackbarProvider>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,5 +1,4 @@
 import {combineReducers} from '@reduxjs/toolkit';
-// import {reducer as toastrReducer} from 'react-redux-toastr'; // TODO: Add toastr
 import consumptionReducer from '../features/consumptions/consumptionSlice';
 import foodReducer from '../features/foods/foodSlice';
 import goalReducer from '../features/goals/goalSlice';
