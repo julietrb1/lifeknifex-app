6bbdd5f0801ff4f0e050c5e41a65e3ed97ff99a0
Dylan Boyd <dylanjboyd@icloud.com>
2020-05-15 00:16:05 +1200

    Add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,9 @@ import store from "./redux/store";
 import {Router} from "react-router-dom";
 import {SnackbarProvider} from "notistack";
 import history from './history';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({dsn: "https://152ac18d2299434b96e9b3b784973578@o153106.ingest.sentry.io/5240603"});
 
 ReactDOM.render(
     <SnackbarProvider maxSnack={1}>
