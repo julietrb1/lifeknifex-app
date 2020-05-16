1a557c542b08802dafbee68ca0310859dd8d21c5
Dylan Boyd <dylanjboyd@icloud.com>
2020-05-17 01:49:02 +1200

    Start playing with themes

--- src/components/common-components/HeaderBar.tsx
+++ src/components/common-components/HeaderBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './HeaderBar.scss';
-import { Header } from 'semantic-ui-react';
 import { APP_TITLE } from '../../constants';
 
 export interface IHeaderBarProps {
@@ -10,7 +9,7 @@ export interface IHeaderBarProps {
 
 const HeaderBar: React.FC<IHeaderBarProps> = ({ title }: IHeaderBarProps) => {
   document.title = title ? `${title} - ${APP_TITLE}` : APP_TITLE;
-  return <Header as="h2" content={title}/>;
+  return <h2>{title}</h2>;
 };
 
 export default HeaderBar;
